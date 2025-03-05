763571c56d27b81eb5d4098f33f164f60ba7203a
hjpark1021 <pp101521@naver.com>
2025-03-05 16:48:54 +0900

    jhkim_updat

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "Deploying updates to GitHub..."
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+./hugo -t hugo-universal-theme # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 cp CNAME docs/
 
 # Add changes to git.
-git add .
+git add -f *
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -17,6 +17,7 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
+
 # Push source and build repos.
+# git push origin +master
 git push -u origin master
-#git push origin +master
